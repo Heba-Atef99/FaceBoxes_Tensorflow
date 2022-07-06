4623d50c729a73294b68d3e74a0bb3e3a7145c42
Heba Atef <hoba30.ha@gmail.com>
2022-07-06 20:56:59 +0200

    add training files

---
+++ Training/create_pb.py
@@ -0,0 +1,48 @@
+import tensorflow as tf
+import argparse
+
+"""Create a .pb frozen inference graph from a SavedModel."""
+
+
+def make_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-s', '--saved_model_folder', type=str
+    )
+    parser.add_argument(
+        '-o', '--output_pb', type=str, default='model.pb'
+    )
+    return parser.parse_args()
+
+
+def main():
+
+    graph = tf.compat.v1.Graph()
+    config = tf.compat.v1.ConfigProto()
+    config.gpu_options.visible_device_list = '0'
+    with graph.as_default():
+        with tf.compat.v1.Session(graph=graph, config=config) as sess:
+            tf.compat.v1.saved_model.loader.load(sess, [tf.compat.v1.saved_model.tag_constants.SERVING], ARGS.saved_model_folder)
+
+            # output ops
+            keep_nodes = ['boxes', 'scores', 'num_boxes']
+
+            input_graph_def = tf.compat.v1.graph_util.convert_variables_to_constants(
+                sess, graph.as_graph_def(),
+                output_node_names=keep_nodes
+            )
+            output_graph_def = tf.compat.v1.graph_util.remove_training_nodes(
+                input_graph_def,
+                protected_nodes=keep_nodes + [n.name for n in input_graph_def.node if 'nms' in n.name]
+            )
+            # ops in 'nms' scope must be protected for some reason,
+            # but why?
+
+            with tf.compat.v1.gfile.GFile(ARGS.output_pb, 'wb') as f:
+                f.write(output_graph_def.SerializeToString())
+            print('%d ops in the final graph.' % len(output_graph_def.node))
+
+
+ARGS = make_args()
+tf.compat.v1.logging.set_verbosity('INFO')
+main()
