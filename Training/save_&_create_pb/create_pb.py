0ae9b785c9a1b0cc625760f006bfc590a1815128
Heba Atef <hoba30.ha@gmail.com>
2022-07-06 21:42:18 +0200

    upload the saving & creating pb file

--- Training/save_&_create_pb/create_pb.py
+++ Training/save_&_create_pb/create_pb.py
@@ -17,32 +17,32 @@ def make_args():
 
 def main():
 
-    graph = tf.compat.v1.Graph()
-    config = tf.compat.v1.ConfigProto()
+    graph = tf.Graph()
+    config = tf.ConfigProto()
     config.gpu_options.visible_device_list = '0'
     with graph.as_default():
-        with tf.compat.v1.Session(graph=graph, config=config) as sess:
-            tf.compat.v1.saved_model.loader.load(sess, [tf.compat.v1.saved_model.tag_constants.SERVING], ARGS.saved_model_folder)
+        with tf.Session(graph=graph, config=config) as sess:
+            tf.saved_model.loader.load(sess, [tf.saved_model.tag_constants.SERVING], ARGS.saved_model_folder)
 
             # output ops
             keep_nodes = ['boxes', 'scores', 'num_boxes']
 
-            input_graph_def = tf.compat.v1.graph_util.convert_variables_to_constants(
+            input_graph_def = tf.graph_util.convert_variables_to_constants(
                 sess, graph.as_graph_def(),
                 output_node_names=keep_nodes
             )
-            output_graph_def = tf.compat.v1.graph_util.remove_training_nodes(
+            output_graph_def = tf.graph_util.remove_training_nodes(
                 input_graph_def,
                 protected_nodes=keep_nodes + [n.name for n in input_graph_def.node if 'nms' in n.name]
             )
             # ops in 'nms' scope must be protected for some reason,
             # but why?
 
-            with tf.compat.v1.gfile.GFile(ARGS.output_pb, 'wb') as f:
+            with tf.gfile.GFile(ARGS.output_pb, 'wb') as f:
                 f.write(output_graph_def.SerializeToString())
             print('%d ops in the final graph.' % len(output_graph_def.node))
 
 
 ARGS = make_args()
-tf.compat.v1.logging.set_verbosity('INFO')
+tf.logging.set_verbosity('INFO')
 main()
---
+++ Training/save_&_create_pb/save.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+import json
+from model import model_fn
+
+
+"""The purpose of this script is to export a savedmodel."""
+
+
+CONFIG = '../config.json'
+OUTPUT_FOLDER = 'export/run02'
+GPU_TO_USE = '0'
+
+WIDTH, HEIGHT = None, None
+# size of an input image,
+# set (None, None) if you want inference
+# for images of variable size
+
+
+tf.logging.set_verbosity('INFO')
+params = json.load(open(CONFIG))
+model_params = params['model_params']
+
+config = tf.ConfigProto()
+config.gpu_options.visible_device_list = GPU_TO_USE
+run_config = tf.estimator.RunConfig()
+run_config = run_config.replace(
+    model_dir=model_params['local_model_dir'],
+    session_config=config
+)
+estimator = tf.estimator.Estimator(model_fn, params=model_params, config=run_config)
+
+
+def serving_input_receiver_fn():
+    images = tf.placeholder(dtype=tf.uint8, shape=[None, HEIGHT, WIDTH, 3], name='image_tensor')
+    features = {'images': tf.to_float(images)*(1.0/255.0)}
+    return tf.estimator.export.ServingInputReceiver(features, {'images': images})
+
+
+estimator.export_savedmodel(
+    OUTPUT_FOLDER, serving_input_receiver_fn
+)
